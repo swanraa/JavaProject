919856a3c411cd2ff0a65cc75d58474196b33cc4
swarnaa <swarna.annamaneni@techmahindra.com>
2016-02-22 12:11:21 +0530

    modified2

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String [] args){
 				
 		System.out.println("Hello. Everone...");
-		System.out.println("Hello...11111111.");
+		System.out.println("Hello...1.");
 		System.out.println("Hello.222222...");
 	}
 }
